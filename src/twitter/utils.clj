475dfb063d066db1da25b5acedc32a1d713912f6
Adam Wynne <adamwynne@gmail.com>
2011-06-17 10:33:22 +0100

    adding some tests

---
+++ src/twitter/utils.clj
@@ -0,0 +1,41 @@
+(ns twitter.utils
+  (:use
+   [clojure.test]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn transform-map
+  "transforms the k/v pairs of a map using a supplied transformation function"
+  [m & {:keys [key-trans val-trans] :or {key-trans (fn [x] x) val-trans (fn [x] x)}}]
+  
+  (into {} (map (fn [[k v]] [(key-trans k) (val-trans v)]) m)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(deftest test-transform-map
+  (is (= (transform-map {:a 0 :b 1 :c 2 :d 3} :key-trans name) {"a" 0, "b" 1, "c" 2, "d" 3}))
+  (is (= (transform-map {:a 0 :b 1 :c 2 :d 3} :val-trans inc) {:a 1 :b 2 :c 3 :d 4})))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn partition-map
+  "partitions a map, depending on a predicate, returning a vector of maps of passes and fails"
+  [map-to-partition pred]
+  
+  (loop [passes {}
+         fails {}
+         m map-to-partition]
+    (if (empty? m) [passes fails]
+        (let [[k v] (first m)]
+          (if (pred [k v])
+            (recur (assoc passes k v) fails (rest m))
+            (recur passes (assoc fails k v) (rest m)))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(deftest test-partition-map
+  (is (= (partition-map {:a 1 :b 2 :c 3 :d 4} (comp even? second)) [{:d 4, :b 2} {:c 3, :a 1}]))
+  (is (= (partition-map {:a 1 :b 2 :c 3 :d 4} (comp (partial < 0) second)) [{:d 4, :c 3, :b 2, :a 1} {}]))
+  (is (= (partition-map {} even?) [{} {}])))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
