6ea366abff85897fc160124d66b8810c4c16369e
Adam Wynne <adamwynne@gmail.com>
2011-06-29 19:22:01 +0100

    working on the readme and cleared the creds in the properties file

--- test/twitter/test/api/streaming.clj
+++ test/twitter/test/api/streaming.clj
@@ -4,7 +4,29 @@
    [twitter.test.creds]
    [twitter.test.utils]
    [twitter.callbacks]
-   [twitter.api.streaming]))
+   [twitter.api.streaming])
+  (:require
+   [http.async.client :as ac])
+  (:import
+   (twitter.callbacks Callbacks)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn streaming-test-callback
+  []
+  (let [embed-status-fn (fn [response] {:status (ac/status response)})]
+    (Callbacks. embed-status-fn embed-status-fn)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(deftest test-streaming
+  (is-200 statuses-filter :params {:track "BhandMeeting"} :callbacks (streaming-test-callback))
+  (is-200 statuses-sample :callbacks (streaming-test-callback)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(deftest test-user-streaming
+  (is-200 user-stream :callbacks (streaming-test-callback)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
