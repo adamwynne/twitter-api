875ef338002ec4665bc3c494b4e8cd0f5a29e5ea
Adam Wynne <adamwynne@gmail.com>
2011-06-30 16:27:47 +0100

    bumped the version number and made it clojars compliant

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject twitter-api "1.0.0-SNAPSHOT"
-  :description "async twitter interface"
-  :dev-dependencies [[org.slf4j/slf4j-simple "1.6.1"]] 
+(defproject twitter-api/twitter-api "0.1.0"
+  :description "full twitter interface"
+  :dev-dependencies [[org.slf4j/slf4j-simple "1.6.1"]
+                     [lein-clojars "0.6.0"]] 
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [http.async.client "0.3.0"]
