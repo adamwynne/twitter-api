9db079d59026fb4f090bbda28324f0be3ff4a309
Adam Wynne <adamwynne@gmail.com>
2011-09-27 13:12:20 +0100

    bump verrsion number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject twitter-api/twitter-api "0.6.1"
+(defproject twitter-api/twitter-api "0.7.0-SNAPSHOT"
   :description "full twitter interface"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"]
