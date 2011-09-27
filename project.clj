548ae29e62c4f308845b9e8791fd951b7e1afa8f
Adam Wynne <adamwynne@gmail.com>
2011-09-27 13:10:36 +0100

    quick patch to change dependencies to a newer version of async.client

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject twitter-api/twitter-api "0.7.0-SNAPSHOT"
+(defproject twitter-api/twitter-api "0.6.1"
   :description "full twitter interface"
-  :dev-dependencies [[lein-difftest "1.3.3"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"]
-                 [org.clojars.adamwynne/http.async.client "0.3.1"]
+                 [org.clojars.adamwynne/http.async.client "0.4.1"]
                  [org.clojars.adamwynne/clj-oauth "1.2.11"]])
