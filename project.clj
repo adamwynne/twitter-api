d625403a4c07e7d740eb0bd4f6d8c138b8c51d7a
Adam Wynne <adamwynne@gmail.com>
2011-12-08 10:50:12 +0000

    updated the clj-oauth library to use the consumer as a defrecord rather than a struct

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject twitter-api/twitter-api "0.6.3"
+(defproject twitter-api/twitter-api "0.6.4"
   :description "full twitter interface"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"]
                  [org.clojars.adamwynne/http.async.client "0.4.1"]
-                 [org.clojars.adamwynne/clj-oauth "1.2.11"]])
+                 [org.clojars.adamwynne/clj-oauth "1.2.12"]])
