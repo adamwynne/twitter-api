3ca5594c5eaefb8e74bbcbd7edd8dfafd7ddf8d9
Adam Wynne <adamwynne@gmail.com>
2012-09-13 14:10:39 +0100

    Changed to the latest http.async.client

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject twitter-api/twitter-api "0.6.10"
+(defproject twitter-api/twitter-api "0.6.11"
   :description "full twitter interface"
   :plugins [[lein-clojars "0.7.0"]
             [lein-swank "1.4.4"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.1"]
-                 [org.clojars.adamwynne/http.async.client "0.4.1"]
+                 [http.async.client "0.4.5"]
                  [org.clojars.adamwynne/clj-oauth "1.2.13"]])
