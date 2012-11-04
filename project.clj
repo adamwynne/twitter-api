b1b94ea16c4d936d150ae19270c887997fc3105f
Benny Tsai <benny.tsai@gmail.com>
2012-11-03 21:59:02 -0700

    bump data.json version

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :plugins [[lein-clojars "0.7.0"]
             [lein-swank "1.4.4"]]
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/data.json "0.1.1"]
+                 [org.clojure/data.json "0.2.1"]
                  [http.async.client "0.4.5"]
                  [org.clojars.adamwynne/clj-oauth "1.2.13"]])
