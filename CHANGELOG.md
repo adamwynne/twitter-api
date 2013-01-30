128bfe8c37281185f9fc07f3530707b4f6db2f8b
Adam Wynne <adamwynne@gmail.com>
2013-01-30 18:03:37 +0000

    Upgraded the async library dependency

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # twitter-api changelog
 
+## 0.7.2
+* upgraded the lein dependency to 2 (and removed the plugins - should be .lein/profiles.clj)
+* upgraded the async library to 0.5.0
+	
 ## 0.7.1
 * bumped the search api version to 1.1 (thanks Aaron Steele)
 * incorporated some recommendations from Kibit (thanks Seymores)
--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
-(defproject twitter-api/twitter-api "0.7.1"
+(defproject twitter-api/twitter-api "0.7.2"
   :description "full twitter interface"
-  :plugins [[lein-clojars "0.7.0"]
-            [lein-swank "1.4.4"]]
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.2.1"]
-                 [http.async.client "0.4.5"]
-                 [org.clojars.adamwynne/clj-oauth "1.2.13"]])
+                 [http.async.client "0.5.0"]
+                 [org.clojars.adamwynne/clj-oauth "1.2.19"]]
+  :min-lein-version "2.0.0")
