f526eb09dd03856101a39a7ac7aef111ed43924c
adamwynne <adamwynne@gmail.com>
2014-10-08 12:26:39 +0100

    new clojars stuff

--- project.clj
+++ project.clj
@@ -1,7 +1,13 @@
 (defproject twitter-api/twitter-api "0.7.7"
-  :description "full twitter interface"
+  :description "full twitter api async interface"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/data.json "0.2.5"]
                  [http.async.client "0.5.2"]
                  [clj-oauth "1.5.1"]]
+  :url "https://github.com/adamwynne/twitter-api"
+  :scm {:name "git"
+        :url "https://github.com/adamwynne/twitter-api"}
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/license/LICENSE-2.0.html"}
+  :deploy-repositories [["clojars" {:sign-releases false}]]
   :min-lein-version "2.0.0")
