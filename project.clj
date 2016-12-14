07dd6c26362563d8d37fc8552e6c3d2fbad874a5
Christopher Brown <io@henrian.com>
2016-12-14 17:56:34 -0600

    Match the project.clj license to the original from the README

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject twitter-api/twitter-api "0.7.9"
-  :description "full twitter api async interface"
+  :description "Async interface to Twitter's REST and Streaming APIs"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.codec "0.1.0"]
                  [http.async.client "0.5.2"]
                  [clj-oauth "1.5.5"]]
   :url "https://github.com/adamwynne/twitter-api"
-  :license {:name "Apache License, Version 2.0"
-            :url "http://www.apache.org/license/LICENSE-2.0.html"}
+  :license {:name "Eclipse Public License"
+            :url "https://www.eclipse.org/legal/epl-v10.html"}
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :sign-releases false}]]
   :min-lein-version "2.0.0")
