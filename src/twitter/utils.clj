35bbc76cbcaa50e7084353176559bd84498b1e9d
Christopher Brown <io@henrian.com>
2017-12-11 23:28:54 -0600

    Remove unused (but tested!) partition-map helper

--- src/twitter/utils.clj
+++ src/twitter/utils.clj
@@ -11,18 +11,6 @@
   (if map-to-transform
     (into {} (map (fn [[k v]] [(key-trans k) (val-trans v)]) map-to-transform))))
 
-(defn partition-map
-  "partitions a map, depending on a predicate, returning a vector of maps of passes and fails"
-  [map-to-partition pred]
-  (loop [passes {}
-         fails {}
-         m map-to-partition]
-    (if (empty? m) [passes fails]
-        (let [[k v] (first m)]
-          (if (pred [k v])
-            (recur (assoc passes k v) fails (rest m))
-            (recur passes (assoc fails k v) (rest m)))))))
-
 (defn get-file-ext
   "retrieves the file extension portion from the filename"
   [filename]
--- test/twitter/test/utils.clj
+++ test/twitter/test/utils.clj
@@ -1,16 +1,11 @@
 (ns twitter.test.utils
   (:require [clojure.test :refer :all]
-            [twitter.utils :refer [get-file-ext partition-map
+            [twitter.utils :refer [get-file-ext
                                    transform-map]]))
 
 (deftest test-transform-map
   (is (= (transform-map {:a 0 :b 1 :c 2 :d 3} :key-trans name) {"a" 0, "b" 1, "c" 2, "d" 3}))
   (is (= (transform-map {:a 0 :b 1 :c 2 :d 3} :val-trans inc) {:a 1 :b 2 :c 3 :d 4})))
-
-(deftest test-partition-map
-  (is (= (partition-map {:a 1 :b 2 :c 3 :d 4} (comp even? second)) [{:d 4, :b 2} {:c 3, :a 1}]))
-  (is (= (partition-map {:a 1 :b 2 :c 3 :d 4} (comp (partial < 0) second)) [{:d 4, :c 3, :b 2, :a 1} {}]))
-  (is (= (partition-map {} even?) [{} {}])))
 
 (deftest test-get-file-ext
   (is (= (get-file-ext "adam.ext") "ext"))
